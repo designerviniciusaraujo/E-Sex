2d49d273c411b271a6faa04ae011a74c35447134
Vinicius Araujo <designerviniciusaraujo@gmail.com>
2017-07-17 15:53:14 -0300

    Sent by DeployFC at 17/07/2017 15:53:14

--- htm/jurosparcelas.js
+++ htm/jurosparcelas.js
@@ -2,14 +2,13 @@
 //var Juros=new Array(0,0,1.99,2.99,3.99);
 
 var Juros=new Array();
-Juros[0]=0; //1x (à vista)
+Juros[0]=0; //1x (ï¿½ vista)
 Juros[1]=0; //2x
 Juros[2]=0; //3x
 Juros[3]=0; //4x
 Juros[4]=0; //5x
 Juros[5]=0; //6x
-Juros[6]=0; //7x
-Juros[7]=0; //8x
-Juros[8]=0; //9x
-Juros[9]=0; //10x
-
+// Juros[6]=0; //7x
+// Juros[7]=0; //8x
+// Juros[8]=0; //9x
+// Juros[9]=0; //10x
